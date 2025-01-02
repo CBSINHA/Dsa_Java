363e54bf70731ee94e39b973f9c5db05fc43f747
Shashank Sinha <shashanksinha590@gmail.com>
2025-01-02 16:07:40 +0530

    Added empty() method in stack implementation

--- Stacks.java
+++ Stacks.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Stacks {
     public static void main(String[] args) {
         Stack<String>soulsborne=new Stack<String>();
+        System.out.println("Is the stack empty?:"+soulsborne.empty());
         soulsborne.push("Demon Souls");
         soulsborne.push("Dark Souls");
         soulsborne.push("Bloodborne");
@@ -11,5 +12,6 @@ public class Stacks {
         System.out.println("Game at the top:"+soulsborne.peek());
         soulsborne.pop();
         System.out.println("After removing top game: "+soulsborne);
+        System.out.println("Is thr Stack empty?:"+soulsborne.empty());
     }
 }
